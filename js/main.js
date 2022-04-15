ccae030ff2352306a16469f4cb4ac5ddd101466b
abdunozir <abduvaliyevabdunozir@gmail.com>
2022-04-15 08:11:18 +0500

    new

---
+++ js/main.js
@@ -0,0 +1,69 @@
+let chess_board = document.querySelector('.chess_board');
+let cards = document.querySelector('.cards');
+let clicked_stone = '';
+
+let card = '';
+for (let i = 0; i < chessObj.length; i++) {
+  let rows = document.createElement('div');
+  rows.className = 'rows';
+  for (let j = 0; j < chessObj[i].length; j++) {
+    card += `<div onmouseover="overCard(event)" onmouseleave="leaveCard(event)" class=" ${chessObj[i][j].id} card${chessObj[i][j].id} card"></div>`;
+  }
+  rows.innerHTML = card;
+  card = '';
+  cards.appendChild(rows);
+}
+
+function overCard(e) {
+  e.target.classList.add('hoveredCard');
+  chessObj.forEach((item) => {
+    item.forEach((el) => {
+      if (e.target.classList[0] == el.id) {
+        document.querySelector('.xCords').innerHTML = el.x;
+        document.querySelector('.yCords').innerHTML = el.y;
+      }
+    });
+  });
+
+  //   when stone clicked
+  if (clicked_stone == 'ot') {
+    shohFunc(e);
+  }
+}
+function leaveCard(e) {
+  e.target.classList.remove('hoveredCard');
+  document.querySelectorAll('.hoveredCard1').forEach((el) => {
+    el.classList.remove('hoveredCard1');
+  });
+}
+
+let stons = document.querySelectorAll('.ston');
+
+// slicked stones
+stons.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    console.log(e.currentTarget);
+    let stone_type =
+      e.currentTarget.classList[e.currentTarget.classList.length - 1];
+    clicked_stone = stone_type;
+    console.log(document.querySelector('.ston_selected'));
+    if (document.querySelector('.ston_selected') !== null) {
+      document
+        .querySelector('.ston_selected')
+        .classList.remove('ston_selected');
+    }
+
+    e.currentTarget.classList.add('ston_selected');
+  });
+});
+
+// let stons = document.querySelectorAll(".stone");
+
+// stons.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     stons.forEach(element => {
+//       element.classList.remove('ston_selected')
+//     })
+//     item.classList.add('ston_selected')
+//   });
+// });
